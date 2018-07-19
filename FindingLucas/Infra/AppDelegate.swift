ac1bf7e173bdd5a3b82e2671a13136e57dcd5a02
Guilherme <guizin.coelho@gmail.com>
2018-07-19 14:08:03 -0300

    First commit

---
+++ FindingLucas/Infra/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FindingLucas
+//
+//  Created by Guilherme Coelho on 19/07/18.
+//  Copyright © 2018 DevForFun_Guilherme_Coelho. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  
+  private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+}
